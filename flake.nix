ac07a724adcab29bc326e71f2bf7b5a2f1510d20
Eren Siegman <eren.siegman@esiegman.dev>
2025-10-23 00:03:02 -0400

    Simplified Flake.nix

--- flake.nix
+++ flake.nix
@@ -3,18 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     flake-utils.url = "github:numtide/flake-utils";
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
@@ -23,52 +13,18 @@
       nixpkgs,
       flake-utils,
       crane,
-      rust-overlay,
       ...
-    }@inputs:
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = [
-          (import rust-overlay)
-          crane.overlays.default
-        ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" ];
-        };
-
-        craneLib = pkgs.crane-lib.overrideToolchain rustToolchain;
-
-        nix-shell-gen = craneLib.buildPackage {
-          pname = "nix-shell-gen";
-          src = craneLib.cleanCargoSource (craneLib.path ./.);
-
-          nativeBuildInputs = [ ];
-        };
-
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
       in
       {
-        packages.default = nix-shell-gen;
-
-        apps.default = flake-utils.lib.mkApp {
-          drv = nix-shell-gen;
-        };
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ nix-shell-gen ];
-
-          packages = with pkgs; [
-            rustToolchain
-            rust-analyzer
-            cargo-watch
-            cargo-edit
-          ];
-
-          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+        packages.default = craneLib.buildPackage {
+          pname = "nix-shell-gen";
+          src = craneLib.cleanCargoSource ./.;
         };
       }
     );
